64598b1feaaeeb9c6097e7afa19d9dc07279a117
celbax95 <loicmace95@gmail.com>
2020-03-02 15:11:25 +0100

    [MENU] Ajout du titre du menu

--- src/fr/state/menu/Menu.java
+++ src/fr/state/menu/Menu.java
@@ -1,13 +1,22 @@
 package fr.state.menu;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.font.TextAttribute;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import fr.inputs.Input;
 import fr.inputs.keyboard.KeyboardEvent;
+import fr.util.point.Point;
 import fr.util.widgets.Widget;
 import fr.util.widgets.WidgetHolder;
+import fr.util.widgets.widget.WElement;
+import fr.util.widgets.widget.data.TextData;
+import fr.util.widgets.widget.drawelements.DELabel;
 import fr.window.WinData;
 
 public class Menu extends WidgetHolder {
@@ -16,13 +25,19 @@ public class Menu extends WidgetHolder {
 		{
 			this.holderName = "menu";
 
-			this.resScheme = "/resources/menu/" + this.holderName + "/*.png";
-
+			this.resScheme = "/resources/" + this.holderName + "/*.png";
 			this.resources = new String[] {
-
-			};
+					"button",
+					"button_pressed",
+					"button_selected",
+					"close",
+					"close_pressed",
+					"largeButton",
+					"largeButton_pressed" };
 		}
 	};
+
+	private static final String TITLE = "MINESWEAPER";
 
 	private MenuState state;
 
@@ -31,15 +46,48 @@ public class Menu extends WidgetHolder {
 	public Menu(MenuState state, WinData wd) {
 		this.state = state;
 		this.winData = wd;
+
+		this.load();
+	}
+
+	private Widget createTitle() {
+		WElement title = new WElement(this);
+
+		Map<TextAttribute, Object> attributes = new HashMap<TextAttribute, Object>() {
+			private static final long serialVersionUID = 1L;
+			{
+				this.put(TextAttribute.TRACKING, 0.02);
+				this.put(TextAttribute.KERNING, TextAttribute.KERNING_ON);
+				this.put(TextAttribute.FAMILY, "Tw Cen MT");
+				this.put(TextAttribute.WEIGHT, TextAttribute.WEIGHT_BOLD);
+				this.put(TextAttribute.SIZE, 88);
+			}
+		};
+
+		Font font = new Font(attributes);
+
+		TextData td = new TextData(new Point(), font, TITLE, new Color(230, 230, 230), 1);
+
+		DELabel txt = new DELabel(new Point(), td);
+		txt.setSupBound(this.winData.getWindowSize());
+
+		title.setDrawElement(txt);
+		title.setPos(new Point(0, 55));
+
+		return title;
 	}
 
 	@Override
 	public List<Widget> createWidgets(List<Widget> widgets) {
+
+		widgets.add(this.createTitle());
+
 		return widgets;
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
+		this.drawWidgets(g);
 	}
 
 	@Override
@@ -62,5 +110,7 @@ public class Menu extends WidgetHolder {
 				System.exit(0);
 			}
 		}
+
+		this.updateWidgets(input);
 	}
 }
