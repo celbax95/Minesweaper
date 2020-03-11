62dd03618d45ba0269e9ffc56e9adfedd44d1050
celbax95 <loicmace95@gmail.com>
2020-03-11 10:09:48 +0100

    [MENU] Mise a jour du bouton de reset des scores au lancement de l'application

--- src/fr/state/menu/Menu.java
+++ src/fr/state/menu/Menu.java
@@ -352,6 +352,7 @@ public class Menu extends WidgetHolder {
 		widgets.add(this.createPlayButton());
 
 		widgets.add(this.createResetScore());
+		this.updateBestScoreLabel();
 
 		return widgets;
 	}
